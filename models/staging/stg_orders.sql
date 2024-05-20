f4ad23055c1d406a42f955dcdad3e7921fd4dc67
eishan.sisodia@impetus.com <eishan.sisodia@impetus.com>
2024-05-20 06:25:39 +0000

    added macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -12,9 +12,12 @@ c.country,
 p.productid,
 p.category,
 p.productname,
-p.subcategory
+p.subcategory,
+{{ markup('o.ORDERSELLINGPRICE','o.ORDERCOSTPRICE') }} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} c
 on o.customerid=c.customerid
 left join {{ ref('raw_product') }} p
 on o.productid=p.productid
+
+{{limit_data_in_dev('ORDERDATE')}}
