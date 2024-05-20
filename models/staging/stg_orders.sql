ffa185c713149f86dc7c457b257e8af311f84fdf
eishan.sisodia@impetus.com <eishan.sisodia@impetus.com>
2024-05-20 11:07:05 +0000

    added delivery team seed to stg orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,6 @@
-select o.ORDERID,
+select 
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} as sk_orders,
+o.ORDERID,
 o.ORDERDATE,
 o.SHIPDATE,
 o.SHIPMODE,
@@ -13,11 +15,14 @@ p.productid,
 p.category,
 p.productname,
 p.subcategory,
-{{ markup('o.ORDERSELLINGPRICE','o.ORDERCOSTPRICE') }} as markup
+{{ markup('o.ORDERSELLINGPRICE','o.ORDERCOSTPRICE') }} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} c
 on o.customerid=c.customerid
 left join {{ ref('raw_product') }} p
 on o.productid=p.productid
+left join {{ ref('delivery_team') }} d
+on o.shipmode=d.shipmode
 
 {{limit_data_in_dev('ORDERDATE')}}
